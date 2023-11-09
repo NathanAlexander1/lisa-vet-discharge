5d256e89123413acc7d742d8f9821c6902fca6ff
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2023-11-08 20:06:08 -0800

    Add additional form input + light styles

--- src/components/Discharge.js
+++ src/components/Discharge.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Discharge(props) {
-  console.log(props);
+  console.log(props.noAbnormalities
+    );
   return (
     <div>
-      <p>It was a pleasure to see {props.petName} today({props.date}), who is obviously a {props.petSex} {props.petType}.</p>
+      <p>
+        It was a pleasure to see {props.petName} today({props.date}), who is
+        obviously a {props.petSex} {props.petType} for {props.reasonForVisit}.
+      </p>
+      {props.noAbnormalities === false ? console.log(false) : <p>Your pet saddly has no abnormalities</p>}
     </div>
   );
 }
--- src/components/StarterForm.js
+++ src/components/StarterForm.js
@@ -6,53 +6,93 @@ function StarterForm() {
   const [petType, setPetType] = useState("");
   const [petSex, setPetSex] = useState("");
   const [date, setDate] = useState("");
+  const [reasonForVisit, setReasonForVisit] = useState("");
+  const [noAbnormalities, setNoAbnormalities] = useState(false);
+
   const [showDischarge, setShowDischarge] = useState(false);
 
   const handleInitialScreenForm = (e) => {
     e.preventDefault();
-    setShowDischarge(true)
+    setShowDischarge(true);
   };
 
   return (
     <div>
-      {!showDischarge ?
-      <form onSubmit={handleInitialScreenForm}>
-        <input
-          name="name"
-          placeholder="Pet Name"
-          value={petName}
-          onChange={(e) => setPetName(e.target.value)}
+      {!showDischarge ? (
+        <form class="initialScreenForm" onSubmit={handleInitialScreenForm}>
+          <input
+            class="form-input"
+            name="name"
+            placeholder="Pet Name"
+            value={petName}
+            onChange={(e) => setPetName(e.target.value)}
+          />
+          <select
+            class="form-input"
+            value={petType}
+            onChange={(e) => setPetType(e.target.value)}
+          >
+            <option value="">Select Pet type</option>
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+            <option value="rabbit">Rabbit</option>
+          </select>
+          <select
+            class="form-input"
+            value={petSex}
+            onChange={(e) => setPetSex(e.target.value)}
+          >
+            <option value="">Select Pet Sex</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <input
+            class="form-input"
+            onChange={(e) => setDate(e.target.value)}
+            type="date"
+            id="start"
+            name="date"
+            value={date}
+            min="2018-01-01"
+            max="2018-12-31"
+          />
+          <select
+            class="form-input"
+            value={reasonForVisit}
+            onChange={(e) => setReasonForVisit(e.target.value)}
+          >
+            <option value="">Select Reason For Visit</option>
+            <option value="a wellness exam">Wellness Exame</option>
+            <option value="a wellness exam and vaccines">
+              Wellness Exam & Vaccines
+            </option>
+            <option value="a pre-surgical exam">Pre-surgical Exam</option>
+            <option value="a post-surgery recheck exam">
+              Post-surgery recheck Exam
+            </option>
+            <option value="a post-adoption exam">Post-adoption Exam</option>
+          </select>
+          <label htmlFor="abnormalities">Abnormalities?</label>
+          <input
+            class="form-input"
+            id="abnormalities"
+            name="abnormalities"
+            onChange={(e) => setNoAbnormalities(e.target.checked)}
+            type="checkbox"
+            checked={noAbnormalities}
+          />
+          <button>Proceed</button>
+        </form>
+      ) : (
+        <Discharge
+          petName={petName}
+          petType={petType}
+          petSex={petSex}
+          date={date}
+          reasonForVisit={reasonForVisit}
+          noAbnormalities={noAbnormalities}
         />
-        <select value={petType} onChange={(e) => setPetType(e.target.value)}>
-          <option value="">Select Pet type</option>
-          <option value="Dog">Dog</option>
-          <option value="Cat">Cat</option>
-          <option value="Rabbit">Rabbit</option>
-        </select>
-        <select value={petSex} onChange={(e) => setPetSex(e.target.value)}>
-          <option value="">Select Pet Sex</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        <input
-          onChange={(e) => setDate(e.target.value)}
-          type="date"
-          id="start"
-          name="date"
-          value={date}
-          min="2018-01-01"
-          max="2018-12-31"
-        />
-        <button>Proceed</button>
-      </form>
-      :
-      <Discharge
-        petName={petName}
-        petType={petType}
-        petSex={petSex}
-        date={date}
-      />
-}
+      )}
     </div>
   );
 }
